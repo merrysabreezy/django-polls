c33862fb0bef10dfb4e26f04acb855af87e9b5f9
merisha77 <merisha@studyinfocentre.com>
2024-08-02 17:24:25 +0545

    register question and choices in admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # "USER": "admin",
+        # "PASSWORD": "password",
+        # "HOST": "127.0.0.1",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
 
-admin.site.register(Question)
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
